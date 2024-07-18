4b3e20713f71d1def2b2564766038ab097644e45
danya <example@gmail.com>
2024-07-19 01:21:44 +0300

    2nd hw is done

--- js/script.js
+++ js/script.js
@@ -10,12 +10,35 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Сколько фильмов вы уже посмотрели?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Сколько фильмов вы уже посмотрели?', ''),
-      d = +prompt('На сколько оцените его?', '');
+// const a = prompt('Какой последний фильм вы смотрели?', ''),
+//       b = +prompt('На сколько оцените его?', ''),
+//       c = prompt('Какой последний фильм вы смотрели?', ''),
+//       d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Какой последний фильм вы смотрели?', ''),
+          b = +prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    };
+};
+const d = personalMovieDB.count
+if (d > 0 && d < 10) {
+    alert('Просмотрено довольно мало фильмов'),
+    console.log(d);
+} else if (d >= 10 && d <= 30) {
+    alert('Вы классический зритель'),
+    console.log(d);
+} else if (d > 30) {
+    alert('Вы киноман'),
+    console.log(d);
+} else {
+    alert('Произошла ошибка')
+}
 
 console.log(personalMovieDB);
